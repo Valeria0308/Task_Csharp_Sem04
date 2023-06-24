c67636d6d047938fca46889ee6407cdad4e54975
Valeria <133519509+Valeria0308@users.noreply.github.com>
2023-06-24 22:54:16 +0300

    added task27

---
+++ Task_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+int SumNumbers(int number)
+{
+    int remainder = 0;
+    int result = 0;
+    while (number > 0)
+    {
+        remainder = number % 10;
+        number /= 10;
+        result += remainder;
+    }
+    return result;
+}
+
+int number = ReadInt("Введите чило: ");
+System.Console.WriteLine($"Сумма чисел в заданноv числе равна: {SumNumbers(number)}");
